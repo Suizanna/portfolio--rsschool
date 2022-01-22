6e695db1f50654a7a7ac264334f3caf0466a24f7
Suizanna <73129581+Suizanna@users.noreply.github.com>
2022-01-22 20:38:31 +0200

    burger

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 console.log(
   "\n 1. Вёрстка валидная +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету +48.\n 4. Требования к css + 12. \n 5. Интерактивность, реализуемая через css +20. \n "
 );
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+const menuWind = document.querySelector(".burg-cont");
+const iconBurg = document.querySelector(".burger");
+const body = document.querySelector(".body");
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+iconBurg.addEventListener(
+  "click",
+  (e = () => {
+    iconBurg.classList.add("hide");
+    menuWind.classList.add("left");
+    body.classList.add("over-hid");
+  })
+);
+const clickMenu = document.querySelectorAll(".cl");
 
-    mobileMenuRef.classList.toggle("is-open");
-  });
-})();
+for (let i = 0; i < clickMenu.length; i++) {
+  clickMenu[i].addEventListener(
+    "click",
+    (e = () => {
+      menuWind.classList.remove("left");
+      iconBurg.classList.remove("hide");
+      body.classList.remove("over-hid");
+    })
+  );
+}
