3de9f52e61bc7f2e325c0e3b76955e26208629c9
Suizanna <73129581+Suizanna@users.noreply.github.com>
2022-01-31 02:13:39 +0200

    add light-theme

--- index.js
+++ index.js
@@ -2,65 +2,6 @@ console.log(
   "\n 1. Вёрстка валидная +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету +48.\n 4. Требования к css + 12. \n 5. Интерактивность, реализуемая через css +20. \n "
 );
 
-// const modal = document.querySelector(".burg-modal");
-// const iconBurg = document.querySelector(".burger-icon");
-// const body = document.querySelector("body");
-// const backdrop = document.querySelector(".backdrop");
-
-// iconBurg.addEventListener("click", onOpenModal);
-
-// function onOpenModal() {
-//   window.addEventListener("keydown", onEscKeyPress);
-//   iconBurg.classList.add("hide");
-//   backdrop.classList.add("backdrop-active");
-//   modal.classList.add("left");
-//   body.classList.add("over-hid");
-// }
-
-// ссылки menu nav
-// const clickMenu = document.querySelectorAll(".cl");
-
-// for (let i = 0; i < clickMenu.length; i++) {
-//   clickMenu[i].addEventListener(
-//     "click",
-//     (e = () => {
-//       modal.classList.remove("left");
-//       backdrop.classList.remove("backdrop-active");
-//       iconBurg.classList.remove("hide");
-//       body.classList.remove("over-hid");
-//     })
-//   );
-// }
-
-// закрытие по backdrop
-// backdrop.addEventListener("click", onBackdropClick);
-
-// function onBackdropClick(event) {
-//   if (event.currentTarget === event.target) {
-//     console.log("Кликнули именно в бекдроп!!!!");
-//     onCloseModal();
-//   }
-// }
-
-// закрытие по "Escape"
-// function onEscKeyPress(event) {
-//   const isEscKey = event.code === "Escape";
-//   console.log(event);
-//   if (isEscKey) {
-//     onCloseModal();
-//   }
-// }
-
-// закрытие Modal
-// function onCloseModal() {
-//   window.removeEventListener("keydown", onEscKeyPress);
-//   modal.classList.remove("left");
-//   backdrop.classList.remove("backdrop-active");
-//   iconBurg.classList.remove("hide");
-// }
-
-//
-
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".header__nav");
 const navLink = document.querySelectorAll(".header__nav-link");
@@ -85,5 +26,56 @@ function onEscKeyPress(event) {
   if (isEscKey) {
     toggleMenu();
     window.removeEventListener("keydown", onEscKeyPress);
+  }
+}
+
+//=================== toggleTheme ==================
+const body = document.querySelector("body");
+const title = document.querySelectorAll(".section-title");
+const btnTheme = document.querySelector(".carbon_sun");
+// const menu = document.querySelector(".header__nav"); вверху есть
+
+btnTheme.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+  body.classList.toggle("light-theme");
+  // menu.classList.toggle("light-theme");
+  console.log("click");
+  // не применяется
+  title.forEach((el) => el.classList.toggle(".light-theme"));
+  if (document.documentElement.clientWidth <= 768) {
+    menu.classList.toggle("light-theme");
+  }
+}
+
+// ====== local storage ==========
+// const languageLink = document.querySelectorAll(".language-link");
+// languageLink.addEventListener("click", );
+
+let lang = en;
+// let theme = light;
+
+function setLocalStorage() {
+  localStorage.setItem("lang", lang);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem("lang")) {
+    const lang = localStorage.getItem("lang");
+    getTranslate(lang);
+  }
+}
+window.addEventListener("load", getLocalStorage);
+//
+// ===============portfolioImages=================
+const portfolioImages = document.querySelectorAll(".portfolio-image");
+
+function changeImage(event) {
+  if (event.target.classList.contains("portfolio-btn")) {
+    // здесь код функции, меняющей src изображений
+    portfolioImages.forEach(
+      (img, index) => (img.src = `./assets/img/winter/${index + 1}.jpg`)
+    );
   }
 }
